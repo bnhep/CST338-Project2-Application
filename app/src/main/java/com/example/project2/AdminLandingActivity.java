95307252eabc7cf5aae782c831604c47ef89b5eb
Brandon Nhep <bnhep@csumb.edu>
2025-04-17 15:42:49 -0700

    Capitalized AddMonstersActivity and AdminLandingActivity file names. Added error handling when user leaves password editText blank.

--- app/src/main/java/com/example/project2/AdminLandingActivity.java
+++ app/src/main/java/com/example/project2/AdminLandingActivity.java
@@ -8,13 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class addMonstersActivity extends AppCompatActivity {
+
+
+public class AdminLandingActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_monsters);
+        setContentView(R.layout.activity_admin_landing);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
