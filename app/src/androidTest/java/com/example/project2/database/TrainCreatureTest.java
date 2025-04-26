69501a1016cdd635f49dadbdfe1ce9284c16e994
Alexis Wogoman <awogoman@csumb.edu>
2025-04-26 12:17:34 -0700

    made second unit test, its ugly and very dumb but i am at wits end

--- app/src/androidTest/java/com/example/project2/database/TrainCreatureTest.java
+++ app/src/androidTest/java/com/example/project2/database/TrainCreatureTest.java
@@ -1,10 +1,15 @@
-package com.example.project2;
-
+package com.example.project2.database;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.example.project2.activities.TrainCreatureActivity;
+
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
+
+import android.content.Context;
 
 @RunWith(AndroidJUnit4.class)
 public class TrainCreatureTest {
@@ -14,6 +19,12 @@ public class TrainCreatureTest {
     public void testCapitalize() {
         String result = trainCreatureActivity.capitalize("speed");
         assertEquals("Speed", result);
+    }
+
+    @Test
+    public void testCapitalize2(){
+        String result = trainCreatureActivity.capitalize("health");
+        assertEquals("Health", result);
     }
 
 }
